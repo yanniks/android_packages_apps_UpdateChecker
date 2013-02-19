a4182b9022340741eb0f5525717381c3f3f186f4
yanniks <kontakt@yanniks.de>
2013-02-19 15:23:13 +0100

    source upload

---
+++ src/de/yanniks/cm_updatechecker/UpdateChecker.java
@@ -0,0 +1,33 @@
+package de.yanniks.cm_updatechecker;
+
+import de.yanniks.cm_updatechecker.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class UpdateChecker extends Activity {
+	private WebView mWebView;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.updatecheck);
+    	mWebView = (WebView) findViewById(R.id.updatecheck);
+    	mWebView.getSettings().setJavaScriptEnabled(true);
+    	mWebView.loadUrl("http://yanniks.de/current-cm10ace.html");
+    	mWebView.setWebViewClient(new LoginClient());
+    }
+	public void download (final View view) {
+    	startActivity (new Intent (this,downloadcurrent.class));
+	}
+    private class LoginClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
---
+++ src/de/yanniks/cm_updatechecker/downloadcurrent.java
@@ -0,0 +1,18 @@
+package de.yanniks.cm_updatechecker;
+
+import de.yanniks.cm_updatechecker.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class downloadcurrent extends Activity {
+	private WebView mWebView;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.downloadcurrent);
+    	mWebView = (WebView) findViewById(R.id.downloadcurrent);
+    	mWebView.loadUrl("http://yanniks.de/cm10-1-ace");
+    }
+}
