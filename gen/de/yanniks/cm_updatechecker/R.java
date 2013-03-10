5b06d173aea1db770006cc6794f3b3a72e6102e1
yanniks <kontakt@yanniks.de>
2013-03-10 20:52:50 +0100

    replaced build through auto read - added root

--- gen/de/yanniks/cm_updatechecker/R.java
+++ gen/de/yanniks/cm_updatechecker/R.java
@@ -9,29 +9,169 @@ package de.yanniks.cm_updatechecker;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int vertical=0x7f010000;
+    }
+    public static final class color {
+        public static final int DemoKitLabelGray=0x7f050001;
+        public static final int DemoKitTextWhite=0x7f050000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background_holo_dark=0x7f020000;
+        public static final int demokit_splash=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher_demokit=0x7f020003;
+        public static final int indicator_button1_off_holo_dark=0x7f020004;
+        public static final int indicator_button1_off_noglow=0x7f020005;
+        public static final int indicator_button1_on_holo_dark=0x7f020006;
+        public static final int indicator_button1_on_noglow=0x7f020007;
+        public static final int indicator_button2_off_holo_dark=0x7f020008;
+        public static final int indicator_button2_off_noglow=0x7f020009;
+        public static final int indicator_button2_on_holo_dark=0x7f02000a;
+        public static final int indicator_button2_on_noglow=0x7f02000b;
+        public static final int indicator_button3_off_holo_dark=0x7f02000c;
+        public static final int indicator_button3_off_noglow=0x7f02000d;
+        public static final int indicator_button3_on_holo_dark=0x7f02000e;
+        public static final int indicator_button3_on_noglow=0x7f02000f;
+        public static final int indicator_button_capacitive_off_holo_dark=0x7f020010;
+        public static final int indicator_button_capacitive_off_noglow=0x7f020011;
+        public static final int indicator_button_capacitive_on_holo_dark=0x7f020012;
+        public static final int indicator_button_capacitive_on_noglow=0x7f020013;
+        public static final int joystick_background=0x7f020014;
+        public static final int joystick_normal_holo_dark=0x7f020015;
+        public static final int joystick_pressed_holo_dark=0x7f020016;
+        public static final int scrubber_control_holo_dark=0x7f020017;
+        public static final int scrubber_horizontal_blue_holo_dark=0x7f020018;
+        public static final int scrubber_horizontal_green_holo_dark=0x7f020019;
+        public static final int scrubber_horizontal_holo_dark=0x7f02001a;
+        public static final int scrubber_horizontal_red_holo_dark=0x7f02001b;
+        public static final int scrubber_vertical_blue_holo_dark=0x7f02001c;
+        public static final int scrubber_vertical_green_holo_dark=0x7f02001d;
+        public static final int scrubber_vertical_red_holo_dark=0x7f02001e;
+        public static final int tab_focused_holo_dark=0x7f02001f;
+        public static final int tab_normal_holo_dark=0x7f020020;
+        public static final int tab_touched_holo_dark=0x7f020021;
+        public static final int toggle_button_off_holo_dark=0x7f020022;
+        public static final int toggle_button_on_holo_dark=0x7f020023;
     }
     public static final class id {
-        public static final int download=0x7f060004;
-        public static final int downloadcurrent=0x7f060000;
-        public static final int textView1=0x7f060001;
-        public static final int textView2=0x7f060002;
-        public static final int updatecheck=0x7f060003;
+        public static final int CheckedTextView01=0x7f08000e;
+        public static final int InOutTabContainer=0x7f08001d;
+        public static final int LED1=0x7f080005;
+        public static final int LED10=0x7f080011;
+        public static final int LED11=0x7f080012;
+        public static final int LED12=0x7f080013;
+        public static final int LED13=0x7f080014;
+        public static final int LED14=0x7f080015;
+        public static final int LED15=0x7f080016;
+        public static final int LED16=0x7f080017;
+        public static final int LED2=0x7f080006;
+        public static final int LED3=0x7f080007;
+        public static final int LED4=0x7f080008;
+        public static final int LED5=0x7f080009;
+        public static final int LED6=0x7f08000a;
+        public static final int LED7=0x7f08000b;
+        public static final int LED8=0x7f08000c;
+        public static final int LED9=0x7f080010;
+        public static final int LEDs1=0x7f080019;
+        public static final int LEDs2=0x7f08001a;
+        public static final int LEDs3=0x7f08001b;
+        public static final int LEDs4=0x7f08001c;
+        public static final int Title1=0x7f080003;
+        public static final int Title2=0x7f08000d;
+        public static final int button1=0x7f080026;
+        public static final int checkedTextView1=0x7f080002;
+        public static final int downloadcurrent=0x7f080000;
+        public static final int imageView1=0x7f080021;
+        public static final int inputLabel=0x7f08001e;
+        public static final int installedversion=0x7f080023;
+        public static final int ledROW2=0x7f08000f;
+        public static final int ledRow1=0x7f080004;
+        public static final int outputContainer=0x7f08001f;
+        public static final int servoLED=0x7f080001;
+        public static final int servoRelay_ref=0x7f080022;
+        public static final int tableRow3=0x7f080018;
+        public static final int textView1=0x7f080020;
+        public static final int textView2=0x7f080024;
+        public static final int updatecheck=0x7f080025;
     }
     public static final class layout {
         public static final int downloadcurrent=0x7f030000;
-        public static final int updatecheck=0x7f030001;
+        public static final int led_button=0x7f030001;
+        public static final int led_buttonblank=0x7f030002;
+        public static final int led_layout=0x7f030003;
+        public static final int led_tablet=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int no_device=0x7f030006;
+        public static final int output_tablet=0x7f030007;
+        public static final int outputcontainer=0x7f030008;
+        public static final int updatecheck=0x7f030009;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int current=0x7f040004;
-        public static final int download=0x7f040000;
-        public static final int installed=0x7f040003;
-        public static final int version=0x7f040002;
+        public static final int LED1Label=0x7f060003;
+        public static final int Title=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int current=0x7f06000c;
+        public static final int download=0x7f060009;
+        public static final int inop=0x7f060006;
+        public static final int installed=0x7f06000b;
+        public static final int no_device=0x7f060002;
+        public static final int off=0x7f060004;
+        public static final int on=0x7f060005;
+        public static final int random_off=0x7f060008;
+        public static final int random_on=0x7f060007;
+        public static final int version=0x7f06000a;
     }
     public static final class style {
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f07000b;
+        public static final int DemoKitButtonDisplay=0x7f070008;
+        public static final int DemoKitControlLabel=0x7f070001;
+        public static final int DemoKitControlLightLowerLabel=0x7f070002;
+        public static final int DemoKitControlLightLowerLabelConst=0x7f070003;
+        public static final int DemoKitLEDButton=0x7f070009;
+        public static final int DemoKitLEDButtonBlank=0x7f07000a;
+        public static final int DemoKitLEDText=0x7f070005;
+        public static final int DemoKitMeasurement=0x7f070004;
+        public static final int DemoKitMeasurementSuffix=0x7f070007;
+        public static final int DemoKitNoDevice=0x7f070006;
+        public static final int DemoKitTextBase=0x7f070000;
     }
+    public static final class xml {
+        public static final int accessory_filter=0x7f040000;
+    }
+    public static final class styleable {
+        /** Attributes that can be used with a com_google_android_DemoKit_Slider.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #com_google_android_DemoKit_Slider_vertical de.yanniks.cm_updatechecker:vertical}</code></td><td></td></tr>
+           </table>
+           @see #com_google_android_DemoKit_Slider_vertical
+         */
+        public static final int[] com_google_android_DemoKit_Slider = {
+            0x7f010000
+        };
+        /**
+          <p>This symbol is the offset where the {@link de.yanniks.cm_updatechecker.R.attr#vertical}
+          attribute's value can be found in the {@link #com_google_android_DemoKit_Slider} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:vertical
+        */
+        public static final int com_google_android_DemoKit_Slider_vertical = 0;
+    };
 }
--- src/de/yanniks/cm_updatechecker/UpdateChecker.java
+++ src/de/yanniks/cm_updatechecker/UpdateChecker.java
@@ -1,33 +1,112 @@
 package de.yanniks.cm_updatechecker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import de.yanniks.cm_updatechecker.R;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class UpdateChecker extends Activity {
-	private WebView mWebView;
     /** Called when the activity is first created. */
+	private WebView mWebView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.updatecheck);
+        TextView tv = (TextView)findViewById(R.id.installedversion);
     	mWebView = (WebView) findViewById(R.id.updatecheck);
     	mWebView.getSettings().setJavaScriptEnabled(true);
     	mWebView.loadUrl("http://yanniks.de/roms/current-cm10ace.html");
     	mWebView.setWebViewClient(new LoginClient());
-    }
-	public void download (final View view) {
-    	startActivity (new Intent (this,downloadcurrent.class));
-	}
-    private class LoginClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+        String input = "getprop |awk -F :  '/build.display.id/ { print $2 }'";
+        execCommandLine(input, tv);}
+
+        void execCommandLine(String command, TextView tv)
+            {
+            Runtime runtime = Runtime.getRuntime();
+            Process proc = null;
+            OutputStreamWriter osw = null;
+            // Running the Script
+            try
+            {
+            proc = runtime.exec("su");
+            osw = new OutputStreamWriter(proc.getOutputStream());
+            osw.write(command);
+            osw.flush();
+            osw.close();
+            }
+            // If return error
+            catch (IOException ex)
+            {
+            // Log error
+            Log.e("execCommandLine()", "Command resulted in an IO Exception: " + command);
+            return;
+            }
+            // Try to close the process
+            finally
+            {
+            if (osw != null)
+            {
+            try
+            {
+            osw.close();
+            }
+            catch (IOException e){}
+            }
+            }
+            try 
+            {
+            proc.waitFor();
+            }
+            catch (InterruptedException e){}
+            // Display on screen if error
+            if (proc.exitValue() != 0)
+            {
+            Log.e("execCommandLine()", "Command returned error: " + command + "\n Exit code: " + proc.exitValue());
+            AlertDialog.Builder builder = new AlertDialog.Builder(UpdateChecker.this);
+            builder.setMessage(command + "\nCould not get root rights!");
+            builder.setNeutralButton("OK", null);
+            AlertDialog dialog = builder.create();
+            dialog.setTitle("Error!");
+            dialog.show(); 
+            }
+            BufferedReader reader = new BufferedReader(
+            new InputStreamReader(proc.getInputStream())); 
+            int read;
+            char[] buffer = new char[4096];
+            StringBuffer output = new StringBuffer();
+            try {
+            while ((read = reader.read(buffer)) > 0) {
+            output.append(buffer, 0, read);
+            }
+            } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            }
+            String exit = output.toString();
+            if(exit != null && exit.length() == 0) {
+            exit = "Cannot read current build";
+            } 
+            tv.setText(exit); 
+            } 
+    	public void downloadcurrent (final View view) {
+        	startActivity (new Intent (this,downloadcurrent.class));
+    	}
+        private class LoginClient extends WebViewClient {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
         }
     }
-}
