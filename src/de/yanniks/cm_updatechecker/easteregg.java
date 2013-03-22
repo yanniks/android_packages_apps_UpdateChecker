e1afc5edab344d1ee2569c50e3f1b6e104de0496
yanniks <kontakt@yanniks.de>
2013-03-22 15:50:18 -0400

    added easteregg

---
+++ src/de/yanniks/cm_updatechecker/easteregg.java
@@ -0,0 +1,13 @@
+package de.yanniks.cm_updatechecker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class easteregg extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.easteregg);
+    }
+}
