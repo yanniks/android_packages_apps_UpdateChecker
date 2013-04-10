38d3b005a84b6c858ba296048703d16a30127a49
yanniks <kontakt@yanniks.de>
2013-04-10 20:40:48 +0200

    updated flash system - requires new builds!

--- src/de/yanniks/cm_updatechecker/flashupdate.java
+++ src/de/yanniks/cm_updatechecker/flashupdate.java
@@ -1,10 +1,12 @@
 package de.yanniks.cm_updatechecker;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class flashupdate extends Activity {
     /** Called when the activity is first created. */
@@ -12,31 +14,16 @@ public class flashupdate extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // flash commands from CMUpdater
-        Process p;  
-        try {  
-           // Preform su to get root privledges  
-           p = Runtime.getRuntime().exec("su");   
-          
-           // Performing commands for flashing...
-           DataOutputStream os = new DataOutputStream(p.getOutputStream());  
-           os.writeBytes("mkdir -p /cache/recovery\n");  
-           os.writeBytes("echo 'boot-recovery' > /cache/recovery/command\n");  
-           os.writeBytes("echo '--update_package=/sdcard/Download/cm-current.zip' >> /cache/recovery/command\n");  
-           os.writeBytes("reboot recovery\n");  
-           os.flush();  
-           try {  
-              p.waitFor();  
-                   if (p.exitValue() != 255) {  
-                      // TODO Code to run on success  
-                   }  
-                   else {  
-                       // TODO Code to run on unsuccessful  
-                   }  
-           } catch (InterruptedException e) {  
-              // TODO Code to run in interrupted exception  
-           }  
-        } catch (IOException e) {  
-           // TODO Code to run in input/output exception  
-        }  
+
+        try {
+            Intent i = new Intent("android.intent.action.MAIN"); 
+            ComponentName n = new 
+            ComponentName("com.cyanogenmod.updater", 
+            "com.cyanogenmod.updater.StartUpdate"); 
+            i.setComponent(n); 
+            startActivity(i); 
+        } catch(ActivityNotFoundException e) {
+        	Toast.makeText(getApplicationContext(), getString(R.string.notnew), Toast.LENGTH_LONG).show();
+        }
     }
 }
