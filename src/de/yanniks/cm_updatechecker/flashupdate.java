b0327c48d27f2d18a774adcf1964ae0a2ea0e2a4
yanniks <kontakt@yanniks.de>
2013-07-24 15:15:04 +0200

    Revert "updated flash system - requires new builds!"
    This reverts commit 38d3b005a84b6c858ba296048703d16a30127a49.
    
    Conflicts:
    	AndroidManifest.xml
    	src/de/yanniks/cm_updatechecker/flashupdate.java

--- src/de/yanniks/cm_updatechecker/flashupdate.java
+++ src/de/yanniks/cm_updatechecker/flashupdate.java
@@ -1,11 +1,16 @@
 package de.yanniks.cm_updatechecker;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+<<<<<<< HEAD
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Intent;
+=======
+>>>>>>> parent of 38d3b00... updated flash system - requires new builds!
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class flashupdate extends Activity {
     /** Called when the activity is first created. */
@@ -13,16 +18,31 @@ public class flashupdate extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // flash commands from CMUpdater
-
-        try {
-            Intent i = new Intent("android.intent.action.MAIN"); 
-            ComponentName n = new 
-            ComponentName("com.cyanogenmod.updater", 
-            "com.cyanogenmod.updater.StartUpdate"); 
-            i.setComponent(n); 
-            startActivity(i); 
-        } catch(ActivityNotFoundException e) {
-        	Toast.makeText(getApplicationContext(), getString(R.string.notnew), Toast.LENGTH_LONG).show();
-        }
+        Process p;  
+        try {  
+           // Preform su to get root privledges  
+           p = Runtime.getRuntime().exec("su");   
+          
+           // Performing commands for flashing...
+           DataOutputStream os = new DataOutputStream(p.getOutputStream());  
+           os.writeBytes("mkdir -p /cache/recovery\n");  
+           os.writeBytes("echo 'boot-recovery' > /cache/recovery/command\n");  
+           os.writeBytes("echo '--update_package=/sdcard/Download/cm-current.zip' >> /cache/recovery/command\n");  
+           os.writeBytes("reboot recovery\n");  
+           os.flush();  
+           try {  
+              p.waitFor();  
+                   if (p.exitValue() != 255) {  
+                      // TODO Code to run on success  
+                   }  
+                   else {  
+                       // TODO Code to run on unsuccessful  
+                   }  
+           } catch (InterruptedException e) {  
+              // TODO Code to run in interrupted exception  
+           }  
+        } catch (IOException e) {  
+           // TODO Code to run in input/output exception  
+        }  
     }
 }
